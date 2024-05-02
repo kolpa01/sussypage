f17582fe38c2afc1d8f2c448d6b986ae51cc7e81
kolpa01 <99541174+kolpa01@users.noreply.github.com>
2024-05-02 11:08:25 +0200

    Update script.js

--- refunds/script.js
+++ refunds/script.js
@@ -14,6 +14,10 @@ function myFunction() {
         let coins = (5 * days) / 30;
         const realCoins = Math.round(coins * 100) / 100
         document.getElementById("textfield").innerHTML = realCoins + " zł";
+    } else if (a === "Morex") {
+        document.getElementById("textfield").innerHTML = '<img src="/sussypage/morex/morexspin.gif">';
+    } else if (a === "kolpa01") {
+        document.getElementById("textfield").innerHTML = 'Czemu tu jesteś<br><img src="morenteeth.png">';
     } else {
         document.getElementById("textfield").innerHTML = "None2";
     };
